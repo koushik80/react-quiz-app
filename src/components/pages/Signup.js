8852bf2f5103fd10049a9e83720deb0f808b4e5a
koushik <koushik.dey@live.com>
2022-10-02 23:00:10 +0300

    createdd small components

---
+++ src/components/pages/Signup.js
@@ -0,0 +1,15 @@
+import Illustration from "../Illustration";
+
+const Signup = () => {
+  return (
+    <>
+      <h1>Create an account</h1>
+      <div className="column">
+         <Illustration />
+
+      </div>
+    </>
+  );
+};
+
+export default Signup;
