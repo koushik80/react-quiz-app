d690713a0b663f3d0da0e043f29a7fb171b832f8
koushik <koushik.dey@live.com>
2022-10-03 12:35:45 +0300

    quiz, miniplayer, progressbar and answers comp

--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -29,7 +29,9 @@ const Signup = () => {
 
           <Checkbox text="I agree to the Terms &amp; Conditions" />
 
-          <Button>Submit Now</Button>
+          <Button>
+            <span>Submit Now</span>
+          </Button>
 
           <div className="info">
             Already have an account? <a href="login.html">Login</a> instead.
