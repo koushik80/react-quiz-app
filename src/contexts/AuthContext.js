bb1d555f50a8adb5f24aec859dc455a6f1fc23fb
koushik <koushik.dey@live.com>
2022-10-07 00:29:51 +0300

    authcontext completed

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,5 +1,12 @@
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
-import React, { useContext, useState } from "react";
+import {
+    createUserWithEmailAndPassword,
+    getAuth,
+    onAuthStateChanged,
+    signInWithEmailAndPassword,
+    signOut,
+    updateProfile
+} from "firebase/auth";
+import React, { useContext, useEffect, useState } from "react";
 import "../firebase";
 
 const AuthContext = React.createContext();
@@ -8,15 +15,56 @@ export function useAuth() {       //this 'useAuth' is self-made custom hook
     return useContext(AuthContext);
 }
 
-export function AuthProvider(children) {
+export function AuthProvider({ children }) {
     const [loading, setLoading] = useState(true);
     const [currentUser, setCurrentUser] = useState();
+
+    useEffect(() => {
+        const auth = getAuth();
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            setCurrentUser(user);
+            setLoading(false);
+        });
+
+        return unsubscribe;
+    }, []);
 
     //signup function
 
     async function signup(email, password, username) {
         const auth = getAuth();
-        await createUserWithEmailAndPassword();
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        // update profile
+        await updateProfile(auth.currentUser, {
+            displayName: username
+        });
+
+        const user = auth.currentUser; //for updating in local storage
+        setCurrentUser({
+            ...user,
+        });
+    }
+
+    //login function
+
+    function login(email, password) {
+        const auth = getAuth();
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    //logout function
+
+    function logout() {
+        const auth = getAuth();
+        return signOut(auth);
+     }
+
+    const value = {
+        currentUser,
+        signup,
+        login,
+        logout,
     }
 
     return (
--- src/firebase.js
+++ src/firebase.js
@@ -3,7 +3,7 @@ import { initializeApp } from "firebase/app";
 // firebase configuration
 
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_APP_KEY,
+  apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
