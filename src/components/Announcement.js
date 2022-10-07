7da242278496c8fd0a831f2463e9c985955f0372
koushik <koushik.dey@live.com>
2022-10-08 02:17:39 +0300

    banner

---
+++ src/components/Announcement.js
@@ -0,0 +1,8 @@
+
+import classes from '../styles/Announcement.module.css';
+
+const Announcement = () => {
+  return <div className={classes.announcement}>Learn with Sumit</div>;
+};
+
+export default Announcement;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,13 @@
+import Announcement from '../Announcement';
 import Videos from "../Videos";
+
 const Home = () => {
-  return <Videos />;
+  return (
+    <>
+    <div className="announceMessage"><Announcement /></div>
+    <Videos />
+  </>
+  );
 };
 
 export default Home;
