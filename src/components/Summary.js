2db37edd96930c941bb6bd8ea52079a9bea67d13
koushik <koushik.dey@live.com>
2022-10-10 16:41:37 +0300

    fetching pics from PEXELS API

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,7 +1,29 @@
-import image from "../assets/images/result.gif";
+import successImage from "../assets/images/error.gif";
+import useFetch from "../hooks/useFetch";
 import classes from "../styles/Summary.module.css";
 
-const Summary = ({score, noq}) => {
+const Summary = ({ score, noq }) => {
+  const getKeyword = () => {
+    if ((score / (noq * 5)) * 100 < 50) {
+      return "failed";
+    } else if ((score / (noq * 5)) * 100 < 75) {
+      return "good";
+    } else if ((score / (noq * 5)) * 100 < 100) {
+      return "Very good";
+    } else {
+      return "Excellent!";
+    }
+  };
+
+  const { loading, error, result } = useFetch(
+    `https://api.pexels.com/v1/search?query=${getKeyword()}&per_page=1`,
+    "GET",
+    {Authorization: process.env.REACT_APP_PEXELS_API_KEY,
+    }
+  );
+
+  const image = result ? result?.photos[0].src.medium : successImage;
+
   return (
     <div className={classes.summary}>
       <div className={classes.point}>
@@ -11,9 +33,15 @@ const Summary = ({score, noq}) => {
         </p>
       </div>
 
-      <div className={classes.badge}>
-        <img src={image} alt="Success" />
-      </div>
+      {loading && <div className={classes.badge}>Loading your badge...</div>}
+
+      {error && <div className={classes.badge}>An error occured!</div>}
+
+      {!loading && !error && (
+        <div className={classes.badge}>
+          <img src={image} alt="Success" />
+        </div>
+      )}
     </div>
   );
 }
